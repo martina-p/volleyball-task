74dfe2ce11174c6db24c9d8726b19cdc65efd743
martina-p <martina.puppi@gmail.com>
2017-03-24 13:51:22 +0100

    start adding images

--- psychExpInit.m
+++ psychExpInit.m
@@ -1,6 +1,6 @@
- %% PsychToolBox parameters
+ %% PTB parameters
 
-    % screen Preferences
+    % Screen Preferences
     Screen('Preference', 'VBLTimestampingMode', 3);%Add this to avoid timestamping problems
     Screen('Preference', 'DefaultFontName', 'Geneva');
     Screen('Preference', 'DefaultFontSize', 30); %fontsize
@@ -12,56 +12,46 @@
     black = [0 0 0]; 
     red= [255 0 0]; 
     
-    % Start PsychToolBox
+    % Start PTB
     screens=Screen('Screens');
     Screen('Preference', 'SkipSyncTests', 2);
     screenNumber=max(screens); % Main screen
-    [win,winRect] = Screen('OpenWindow',screenNumber,black); 
+    [win,winRect] = Screen('OpenWindow',screenNumber,black);
+    winRect = [0,0,800,600];
     HideCursor;
 
 
 %% Coordinates
-    % Size of stimuli
-    xstim = 125; % from the stimuli files
-    ystim = 125; % from the stimuli files 
-    % Center location
-    [xc, yc] = RectCenterd(winRect); % Get coordinates of screen center 
-    xcPourEuro = xc - xstim/2-50;
-    ycPourEuro = yc - ystim;
-    xcPourMark = xc-15;
-    ycPourMark = yc + ystim;
-    ycObsMark = yc - ystim;
-    % Distance between center and stimuli 
-    decalageX = 300;
-    decalageY = -250;
-    % Rectangles definition
-    rectCentre = [xc-xstim/2, yc-ystim/2, xc-xstim/2, yc-ystim/2];
-    rectStim = [0, 0, xstim, ystim] + rectCentre; %specify position of stimuli square
-    rectFeed = [0, decalageY, xstim, ystim+decalageY] + rectCentre; %specify the position of the feedback square
-    rectDecalageX = [decalageX, 0, decalageX, 0]; % Translation on the x axis (300 0 300 0)
-    rectDecalageY = [0, decalageY, 0, decalageY]; % Translation on the x axis (300 0 300 0)
-
-    screenshotrect = [xc-xc/2, yc-yc/2+decalageY/2, xc+xc/2, yc+yc/2+decalageY/2];
+    [xc, yc] = RectCenterd(winRect);    %Get coordinates of screen center 
+    xOffsetLeft = xc+300;               %for left image
+    xOffsetRight = xc-300;              %for right image
+    yOffset = 450;
     
 %% keyboard parameters
     KbName('UnifyKeyNames'); % Use same key codes across operating systems for better portability % usa questo
     
-%% loading exp images (supposed to be in the same directory as the script)
-%Load images
-Lose=imread(fullfile('Stimfiles', 'Lose.png')); 
-texLose = Screen('MakeTexture', win, Lose);
-Win=imread(fullfile('Stimfiles', 'Win.png')); 
-texWin = Screen('MakeTexture', win, Win);
-Play=imread(fullfile('Stimfiles', 'Play.png')); 
-texPlay = Screen('MakeTexture', win, Play);
-Pause=imread(fullfile('Stimfiles', 'Pause.png')); 
-texPause = Screen('MakeTexture', win, Pause);
+%% Load images
 
-%% loading stimulus images
-%stim_A{1}=uint8(imread(fullfile('Stimfiles', 'Stim111.bmp'))); % allocate/order stimuli  #session #pair(randomized) #stim(randomized between 1 and 2)
-%texStim_A{1}=Screen('MakeTexture', win, stim_A{1});
-        
-%stim_B{1}=uint8(imread(fullfile('Stimfiles', 'Stim112.bmp'))); % allocate/order stimuli  #session #pair(randomized) #stim(randomized between 1 and 2)
-%texStim_B{1}=Screen('MakeTexture', win, stim_B{1});
+    Lose=imread(fullfile('Stimfiles', 'Lose.png')); 
+    texLose = Screen('MakeTexture', win, Lose);
+    Win=imread(fullfile('Stimfiles', 'Win.png')); 
+    texWin = Screen('MakeTexture', win, Win);
+    Play=imread(fullfile('Stimfiles', 'Play.png')); 
+    texPlay = Screen('MakeTexture', win, Play);
+    Pause=imread(fullfile('Stimfiles', 'Pause.png')); 
+    texPause = Screen('MakeTexture', win, Pause);
 
-% ========= DISPLAY FUNCTIONS  ========= %
+    [imagePlayHeight, imagePlayWidth, colorChannels]= size(Play);
+    [imagePauseHeight, imagePauseWidth, colorChannels]= size(Pause);
+    [imageLoseHeight, imageLoseWidth, colorChannels]= size(Lose);
+    [imageWinHeight, imageWinWidth, colorChannels]= size(Win);
+    
+    %Position play_pause
+    imagePlayPauseRectLeft = [xOffsetLeft, yOffset, xOffsetLeft+imagePlayWidth, yOffset+imagePlayHeight];
+    imagePlayPauseRectRight = [xOffsetRight, yOffset, xOffsetRight+imagePauseWidth, yOffset+imagePauseHeight];
+    Screen('DrawTexture', win, texPlay,[],imagePlayPauseRectLeft);
+    Screen('DrawTexture', win, texPause,[],imagePlayPauseRectRight);
+    Screen('Flip',win);
+
+    %Position pause_play
+
